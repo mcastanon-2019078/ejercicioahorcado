f4efc4f28c24d80f41382a7e2f11e199468d65da
mcastanon-2019078 <135753384+mcastanon-2019078@users.noreply.github.com>
2024-01-18 17:28:14 -0600

    Cuarta Actualizacion

--- Ejercicio Ahorcado 6TO/ahorcado.js
+++ Ejercicio Ahorcado 6TO/ahorcado.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let palabras = ["REPOSITORIO", "GITHUB", "JAVASCRIPT", "HTML", "NETBEANS", "ARMAS", "CABRERA", "ESTUCHE", "CARGADOR", ""];
+    let palabras = ["REPOSITORIO", "GITHUB", "JAVASCRIPT", "HTML", "NETBEANS", "ARMAS", "CABRERA", "ESTUCHE", "CARGADOR"];
     let palabra = palabras[Math.floor(Math.random() * palabras.length)];
     let adElement = document.getElementById("ad");
     let horcar = document.getElementById("horcadocanva").getContext("2d");
@@ -8,5 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let letrasIncorrectas = [];
     let intentosFallidos = 0;
 
-    
+    function actualizarPalabraOculta() {
+        adElement.innerHTML = "";
+        for (let i = 0; i < palabra.length; i++) {
+          let span = document.createElement("span");
+          span.textContent = palabraOculta[i];
+          adElement.appendChild(span);
+        }
+      }
+
+      
 });
